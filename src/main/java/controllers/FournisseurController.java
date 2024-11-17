a6b3d804c7087fd25181da82e6a38ffe2a057829
Anli omar <omaranli56@gmail.com>
2024-11-17 13:35:30 +0400

    finish switch to scene btween add fournisseur form and fournisseur table view

--- src/main/java/controllers/FournisseurController.java
+++ src/main/java/controllers/FournisseurController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.scene.control.*;
 import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
 import utils.SessionManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
@@ -15,12 +16,12 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class FournisseurController {
-
-    // Variable pour savoir si on est dans la scène Fournisseur ou NewFournisseur
-    private boolean isFournisseurSceneActive = true;
-
+    // Initialisation de l'instance de Utils pour charger une nouvelle scène
+    Utils sceneLoader = new Utils();
     @FXML
     public TableView<Fournisseur> tableFournisseurs;
+    @FXML
+    public AnchorPane newFournisseurForm;
 
     @FXML
     private TableColumn<Fournisseur, Integer> colId;
@@ -49,53 +50,63 @@ public class FournisseurController {
     @FXML
     private Button NewFournisseurButton;
 
+
+    @FXML
+    private Button AllFournisseurButton;
+
     @FXML
     public void initialize() {
-        // Si on est dans la scène Fournisseur, on initialise toutes les colonnes
-        if (isFournisseurSceneActive) {
-            colId.setCellValueFactory(new PropertyValueFactory<>("id"));
-            colPays.setCellValueFactory(new PropertyValueFactory<>("pays"));
-            colNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
-            colTelephone.setCellValueFactory(new PropertyValueFactory<>("telephone"));
-            colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
-            colAdresse.setCellValueFactory(new PropertyValueFactory<>("adresse"));
-
-            // Exemple d'ajout de fournisseurs dans la table
-            tableFournisseurs.getItems().add(new Fournisseur(1, "Pays A", "Fournisseur A", "123456789", "emailA@test.com", "Adresse A"));
-            tableFournisseurs.getItems().add(new Fournisseur(2, "Pays B", "Fournisseur B", "987654321", "emailB@test.com", "Adresse B"));
-        } else {
-            // Si on est dans NewFournisseur, on cache certaines colonnes
-            colId.setVisible(false);    // Masquer la colonne "Id"
-            colPays.setVisible(false);  // Masquer la colonne "Pays"
-            colNom.setVisible(false);   // Masquer la colonne "Nom"
-            colTelephone.setVisible(false);  // Masquer la colonne "Téléphone"
-            colEmail.setVisible(false);      // Masquer la colonne "Email"
-            colAdresse.setVisible(false);    // Masquer la colonne "Adresse"
+        // display autenticated user username in sidebar
+        UsernameLabel.setText(SessionManager.getIdentifiant());
+        if (tableFournisseurs != null) {
+            initializeTable();
         }
 
-        // Affichage du nom d'utilisateur
-        UsernameLabel.setText(SessionManager.getIdentifiant());
+        if (newFournisseurForm != null) {
+            initializeForm();
+        }
     }
 
-    public void setFournisseurSceneActive(boolean isActive) {
-        this.isFournisseurSceneActive = isActive;
-    }
+    // Initialisation du tableau des fournisseurs
+    private void initializeTable() {
+        // Initialisation columns
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        colPays.setCellValueFactory(new PropertyValueFactory<>("pays"));
+        colTelephone.setCellValueFactory(new PropertyValueFactory<>("telephone"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colAdresse.setCellValueFactory(new PropertyValueFactory<>("adresse"));
 
-    // Méthode pour ajouter un fournisseur dans la TableView
+        tableFournisseurs.getItems().add(new Fournisseur(1, "Pays A", "Fournisseur A", "123456789", "emailA@test.com", "Adresse A"));
+        tableFournisseurs.getItems().add(new Fournisseur(2, "Pays B", "Fournisseur B", "987654321", "emailB@test.com", "Adresse B"));
+    }
+    /**
+     * method to add new fournisseur in table view
+     * @param fournisseur
+     */
     public void ajouterFournisseur(Fournisseur fournisseur) {
         tableFournisseurs.getItems().add(fournisseur);
     }
 
+
+    // Initialisation of new fournisseur form
+    private void initializeForm() {
+        // Initialisation des champs du formulaire
+        System.out.println("Formulaire d'ajout de fournisseur initialisé");
+    }
+
+    // switch to dashboard scene
     public void DashboardButtonOnAction(ActionEvent actionEvent) throws IOException, SQLException {
-        // Initialisation de l'instance de Utils pour charger une nouvelle scène
-        Utils sceneLoader = new Utils();
-        // Changer de scène pour le Dashboard après avoir cliqué sur le bouton
         sceneLoader.loadScene("Dashboard.fxml", "Dashboard", DashboardButton);
     }
 
+    // switch add new fournisseur form
     public void NewFournisseurOnAction(ActionEvent actionEvent) throws IOException {
-        setFournisseurSceneActive(false); // Activer la scène NewFournisseur
-        Utils sceneLoader = new Utils();
-        sceneLoader.loadScene("NewFournisseur.fxml", "New fournisseur", NewFournisseurButton);
+        sceneLoader.loadScene("NewFournisseur.fxml", "Nounveau fournisseur", NewFournisseurButton);
+    }
+
+    // return to fournisseur table view
+    public void AllFournisseurButtonOnAction(ActionEvent actionEvent) throws IOException {
+        sceneLoader.loadScene("Fournisseurs.fxml", "Tous les fournisseurs", AllFournisseurButton);
     }
 }
